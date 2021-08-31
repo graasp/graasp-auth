96b4bffda56ecfa15fe06c635e433b5fe0739cd2
Julien Torrent <Julien.torrent@outlook.com>
2021-08-31 12:39:57 +0200

    fix: change message to "already exists"

--- src/config/messages.js
+++ src/config/messages.js
@@ -3,7 +3,7 @@ export const SIGN_IN_INVALID_MESSAGE = 'This user doesn\'t exist. Please registe
 export const SIGN_IN_SUCCESS_MESSAGE =
   'You successfully signed in. You will receive an email.';
 export const SIGN_UP_ERROR_MESSAGE = 'There was an error signing up.';
-export const SIGN_UP_DUPLICATE_MESSAGE = 'This user exist. Please sign in.';
+export const SIGN_UP_DUPLICATE_MESSAGE = 'This user already exists. Please sign in.';
 export const SIGN_UP_SUCCESS_MESSAGE =
   'You successfully signed up. You will receive an email.';
 export const EMAIL_ERROR = 'Please enter a valid email address';
