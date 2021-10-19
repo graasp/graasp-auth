fd13404c912bb32b16f381e53ee441f4f4209758
pyphilia <pyphilia@gmail.com>
2021-10-19 18:20:07 +0200

    refactor: trigger notification on error

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUp from './SignUp';
-import { SIGN_UP_PATH, buildSignInPath } from '../config/paths';
+import { SIGN_UP_PATH, buildSignInPath, HOME_PATH } from '../config/paths';
 import SignIn from './SignIn';
 
 const App = () => (
@@ -9,6 +9,7 @@ const App = () => (
     <Switch>
       <Route path={buildSignInPath()} component={SignIn} />
       <Route path={SIGN_UP_PATH} exact component={SignUp} />
+      <Route path={HOME_PATH} component={SignIn} />
     </Switch>
   </Router>
 );
