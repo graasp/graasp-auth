d641a81e5508cf95876a89170e3a89fbf26d17cd
pyphilia <pyphilia@gmail.com>
2023-02-17 09:26:50 +0100

    refactor: fix tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,18 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // `on` is used to hook into various events Cypress emits
+      // `config` is the resolved Cypress config
+
+      const newConfig = {
+        ...config,
+        env: {
+          API_HOST: process.env.REACT_APP_API_HOST,
+        },
+      };
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require('@cypress/code-coverage/task')(on, newConfig);
+      return newConfig;
     },
     baseUrl: 'http://localhost:3001',
   },
