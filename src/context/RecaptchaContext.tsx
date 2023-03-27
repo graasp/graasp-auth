9f5a57c8d424dc70e3bc7dc116e9bda14cce0481
spaenleh <spaenleh@gmail.com>
2023-03-27 11:11:45 +0200

    fix: script should not be attached programatically

--- src/context/RecaptchaContext.tsx
+++ src/context/RecaptchaContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext } from 'react';
 
 declare global {
   interface Window {
@@ -11,16 +11,16 @@ declare global {
   }
 }
 
-const RECAPTCHA_SCRIPT_ID = 'recaptcha-key';
+// const RECAPTCHA_SCRIPT_ID = 'recaptcha-key';
 
 type RecaptchaContextType = {
   executeCaptcha: (action: string) => Promise<string>;
-  isReady: boolean;
+  // isReady: boolean;
 };
 
 const RecaptchaContext = createContext<RecaptchaContextType>({
   executeCaptcha: () => Promise.resolve(''),
-  isReady: false,
+  // isReady: false,
 });
 
 type Props = {
@@ -29,50 +29,50 @@ type Props = {
 };
 
 export const RecaptchaProvider = ({ children, siteKey }: Props) => {
-  const [isReady, setIsReady] = useState(false);
+  // const [isReady, setIsReady] = useState(false);
 
-  /**
-   * Extra security measure to check if the script has
-   * already been included in the DOM
-   */
-  const scriptAlreadyExists = () =>
-    document.querySelector(`script#${RECAPTCHA_SCRIPT_ID}`) !== null;
+  // /**
+  //  * Extra security measure to check if the script has
+  //  * already been included in the DOM
+  //  */
+  // const scriptAlreadyExists = () =>
+  //   document.querySelector(`script#${RECAPTCHA_SCRIPT_ID}`) !== null;
 
-  /**
-   * Append the script to the document.
-   * Whenever the script has been loaded it will
-   * set the isLoaded state to true.
-   */
-  const addRecaptchaScript = () => {
-    const script = document.createElement('script');
-    script.id = RECAPTCHA_SCRIPT_ID;
-    script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`;
-    script.type = 'text/javascript';
-    script.async = true;
-    script.defer = true;
-    script.onload = () => setIsReady(true);
-    document.body.append(script);
-  };
+  // /**
+  //  * Append the script to the document.
+  //  * Whenever the script has been loaded it will
+  //  * set the isLoaded state to true.
+  //  */
+  // const addRecaptchaScript = () => {
+  //   const script = document.createElement('script');
+  //   script.id = RECAPTCHA_SCRIPT_ID;
+  //   script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`;
+  //   script.type = 'text/javascript';
+  //   script.async = true;
+  //   script.defer = true;
+  //   script.onload = () => setIsReady(true);
+  //   document.body.append(script);
+  // };
 
-  /**
-   * Runs first time when component is mounted
-   * and adds the script to the document.
-   */
-  useEffect(
-    () => {
-      if (!scriptAlreadyExists()) {
-        addRecaptchaScript();
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  );
+  // /**
+  //  * Runs first time when component is mounted
+  //  * and adds the script to the document.
+  //  */
+  // useEffect(
+  //   () => {
+  //     if (!scriptAlreadyExists()) {
+  //       addRecaptchaScript();
+  //     }
+  //   },
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   [],
+  // );
 
   const executeCaptcha = (action: string) => {
     return window.grecaptcha.execute(siteKey, { action });
   };
 
-  const value = { executeCaptcha, isReady };
+  const value = { executeCaptcha };
   return (
     <RecaptchaContext.Provider value={value}>
       {children}
