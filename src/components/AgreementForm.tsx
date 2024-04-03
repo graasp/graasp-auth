b2e3f5cdadafe75fc241403d13ce7b09c7ffaf07
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-04-03 09:13:23 +0200

    feat: update the checkboxes labels

--- src/components/AgreementForm.tsx
+++ src/components/AgreementForm.tsx
@@ -37,21 +37,22 @@ export const AgreementForm = ({ onChange }: Props) => {
   return (
     <FormGroup>
       <AgreementCheckbox
-        label={t(AUTH.PRIVACY_POLICY_CHECKBOX_LABEL)}
-        link={PRIVACY_POLICY_LINK}
-        linkLabel={t(AUTH.PRIVACY_POLICY_LINK_LABEL)}
-        isChecked={isPrivacyPolicyChecked}
-        onChange={(isChecked) => privacyPolicyChanged(isChecked)}
-        dataCy={SIGN_UP_PRIVACY_POLICY_CHECKBOX_ID}
-      />
-
-      <AgreementCheckbox
         label={t(AUTH.TERMS_OF_SERVICE_CHECKBOX_LABEL)}
         link={TERMS_OF_SERVICE_LINK}
         linkLabel={t(AUTH.TERMS_OF_SERVICE_LINK_LABEL)}
         isChecked={isTermsOfServiceChecked}
         onChange={(isChecked) => termsOfServiceChanged(isChecked)}
         dataCy={SIGN_UP_TERM_OF_SERVICE_CHECKBOX_ID}
+      />
+      <AgreementCheckbox
+        label={t(AUTH.PRIVACY_POLICY_CHECKBOX_LABEL, {
+          SIGN_UP_BUTTON: t(AUTH.SIGN_UP_BUTTON),
+        })}
+        link={PRIVACY_POLICY_LINK}
+        linkLabel={t(AUTH.PRIVACY_POLICY_LINK_LABEL)}
+        isChecked={isPrivacyPolicyChecked}
+        onChange={(isChecked) => privacyPolicyChanged(isChecked)}
+        dataCy={SIGN_UP_PRIVACY_POLICY_CHECKBOX_ID}
       />
     </FormGroup>
   );
