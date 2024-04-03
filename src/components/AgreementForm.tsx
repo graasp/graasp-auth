3a743aebcc15ed2bd88b5757a46a759750d0d2df
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-04-03 11:26:02 +0200

    feat: merge checkboxes and redirect to landing page of user's language

--- src/components/AgreementForm.tsx
+++ src/components/AgreementForm.tsx
@@ -1,18 +1,16 @@
-import { useState } from 'react';
-
-import { FormGroup } from '@mui/material';
+import { Trans } from 'react-i18next';
 
 import {
-  PRIVACY_POLICY_LINK,
-  TERMS_OF_SERVICE_LINK,
-} from '../config/constants';
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Typography,
+} from '@mui/material';
+
 import { useAuthTranslation } from '../config/i18n';
-import {
-  SIGN_UP_PRIVACY_POLICY_CHECKBOX_ID,
-  SIGN_UP_TERM_OF_SERVICE_CHECKBOX_ID,
-} from '../config/selectors';
+import { SIGN_UP_TERM_OF_SERVICE_CHECKBOX_ID } from '../config/selectors';
+import { useAgreementForm } from '../hooks/useAgreementForm';
 import { AUTH } from '../langs/constants';
-import { AgreementCheckbox } from './AgreementCheckbox';
 
 type Props = {
   onChange: (areAllChecked: boolean) => void;
@@ -20,39 +18,57 @@ type Props = {
 
 export const AgreementForm = ({ onChange }: Props) => {
   const { t } = useAuthTranslation();
-
-  const [isPrivacyPolicyChecked, setIsPrivacyPolicyChecked] = useState(false);
-  const [isTermsOfServiceChecked, setIsTermsOfServiceChecked] = useState(false);
-
-  const privacyPolicyChanged = (isChecked: boolean) => {
-    setIsPrivacyPolicyChecked(isChecked);
-    onChange(isChecked && isTermsOfServiceChecked);
-  };
-
-  const termsOfServiceChanged = (isChecked: boolean) => {
-    setIsTermsOfServiceChecked(isChecked);
-    onChange(isChecked && isPrivacyPolicyChecked);
-  };
+  const {
+    areTermsChecked,
+    handleChanges,
+    privacyPolicyLink,
+    termsOfServiceLink,
+  } = useAgreementForm({ onChange });
 
   return (
     <FormGroup>
-      <AgreementCheckbox
-        label={t(AUTH.TERMS_OF_SERVICE_CHECKBOX_LABEL)}
-        link={TERMS_OF_SERVICE_LINK}
-        linkLabel={t(AUTH.TERMS_OF_SERVICE_LINK_LABEL)}
-        isChecked={isTermsOfServiceChecked}
-        onChange={(isChecked) => termsOfServiceChanged(isChecked)}
-        dataCy={SIGN_UP_TERM_OF_SERVICE_CHECKBOX_ID}
-      />
-      <AgreementCheckbox
-        label={t(AUTH.PRIVACY_POLICY_CHECKBOX_LABEL, {
-          SIGN_UP_BUTTON: t(AUTH.SIGN_UP_BUTTON),
-        })}
-        link={PRIVACY_POLICY_LINK}
-        linkLabel={t(AUTH.PRIVACY_POLICY_LINK_LABEL)}
-        isChecked={isPrivacyPolicyChecked}
-        onChange={(isChecked) => privacyPolicyChanged(isChecked)}
-        dataCy={SIGN_UP_PRIVACY_POLICY_CHECKBOX_ID}
+      <FormControlLabel
+        checked={areTermsChecked}
+        onChange={(_, checked) => handleChanges(checked)}
+        required
+        control={
+          <Checkbox
+            data-cy={SIGN_UP_TERM_OF_SERVICE_CHECKBOX_ID}
+            size="small"
+          />
+        }
+        label={
+          <Typography display="inline" fontSize="small">
+            <Trans
+              i18nKey={AUTH.USER_AGREEMENTS_CHECKBOX_LABEL}
+              values={{
+                sign_up_btn: t(AUTH.SIGN_UP_BUTTON),
+                terms_of_service: t(AUTH.USER_AGREEMENTS_TERMS_OF_SERVICE),
+                privacy_policy: t(AUTH.USER_AGREEMENTS_PRIVACY_POLICY),
+              }}
+              components={[
+                <strong key="sign_up_btn"></strong>,
+                <a
+                  key="terms_of_service_link"
+                  href={termsOfServiceLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  _
+                </a>,
+                <a
+                  key="privacy_policy_link"
+                  href={privacyPolicyLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  _
+                </a>,
+              ]}
+              t={t}
+            />
+          </Typography>
+        }
       />
     </FormGroup>
   );
