a4949538e56517518fd6cb301a15060371b72720
spaenleh <spaenleh@gmail.com>
2024-07-15 09:26:54 +0200

    fix: update route functions

--- cypress/support/server.ts
+++ cypress/support/server.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 
 import { API_ROUTES } from '@graasp/query-client';
 
-const { buildGetMember, GET_CURRENT_MEMBER_ROUTE } = API_ROUTES;
+const { buildGetCurrentMemberRoute } = API_ROUTES;
 
 // use simple id format for tests
 export const ID_FORMAT = '(?=.*[0-9])(?=.*[a-zA-Z])([a-z0-9-]+)';
@@ -22,7 +22,7 @@ export const mockGetCurrentMember = (
   cy.intercept(
     {
       method: 'get',
-      url: `${API_HOST}/${GET_CURRENT_MEMBER_ROUTE}`,
+      url: `${API_HOST}/${buildGetCurrentMemberRoute()}`,
     },
     ({ reply }) => {
       if (shouldThrowError) {
@@ -36,57 +36,6 @@ export const mockGetCurrentMember = (
       return reply({ statusCode: StatusCodes.OK, body: currentMember });
     },
   ).as('getCurrentMember');
-};
-
-export const mockGetMember = (members) => {
-  cy.intercept(
-    {
-      method: 'get',
-      url: new RegExp(`${API_HOST}/${buildGetMember(ID_FORMAT)}$`),
-    },
-    ({ url, reply }) => {
-      const memberId = url.slice(API_HOST.length).split('/')[2];
-      const member = members.find(({ id: mId }) => mId === memberId);
-
-      // member does not exist in db
-      if (!member) {
-        return reply({
-          statusCode: StatusCodes.NOT_FOUND,
-        });
-      }
-
-      return reply({
-        body: member,
-        statusCode: StatusCodes.OK,
-      });
-    },
-  ).as('getMember');
-};
-
-export const mockGetMembers = (members) => {
-  cy.intercept(
-    {
-      method: 'get',
-      url: `${API_HOST}/members?id=`,
-    },
-    ({ url, reply }) => {
-      const memberIds = new URLSearchParams(url).getAll('id');
-      const allMembers = (memberIds as string[])?.map((id) =>
-        members.find(({ id: mId }) => mId === id),
-      );
-      // member does not exist in db
-      if (!allMembers) {
-        return reply({
-          statusCode: StatusCodes.NOT_FOUND,
-        });
-      }
-
-      return reply({
-        body: allMembers,
-        statusCode: StatusCodes.OK,
-      });
-    },
-  ).as('getMembers');
 };
 
 export const mockGetStatus = (shouldThrowServerError = false) => {
