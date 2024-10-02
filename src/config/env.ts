045da21a728ae68ac63acb5edf55d5f18b67edd7
spaenleh <spaenleh@gmail.com>
2024-10-02 17:02:37 +0200

    fix: update env vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,12 +4,6 @@ export const APP_VERSION = import.meta.env.VITE_VERSION;
 export const API_HOST =
   import.meta.env.VITE_GRAASP_API_HOST || 'http://localhost:3000';
 
-export const GRAASP_BUILDER_HOST =
-  import.meta.env.VITE_GRAASP_BUILDER_HOST ?? 'http://localhost:3111';
-
-export const AUTHENTICATION_HOST =
-  import.meta.env.VITE_GRAASP_AUTH_HOST || 'http://localhost:3001';
-
 export const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 
 export const SENTRY_ENV = import.meta.env.VITE_SENTRY_ENV;
