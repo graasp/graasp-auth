132b77534863b372abc51bc09e1070a98d2c7adc
spaenleh <spaenleh@gmail.com>
2024-10-21 09:25:48 +0200

    fix: magic link login to use enter to validate

--- cypress/e2e/SignIn.cy.ts
+++ cypress/e2e/SignIn.cy.ts
@@ -1,4 +1,8 @@
 import { SIGN_IN_PATH } from '../../src/config/paths';
+import {
+  MAGIC_LINK_EMAIL_FIELD_ID,
+  SUCCESS_CONTENT_ID,
+} from '../../src/config/selectors';
 import { MEMBERS } from '../fixtures/members';
 
 describe('Name and Email Validation', () => {
@@ -12,5 +16,16 @@ describe('Name and Email Validation', () => {
     cy.signInByMailAndCheck(WRONG_EMAIL);
     // Signing in with a valid email
     cy.signInByMailAndCheck(GRAASP);
+  });
+});
+
+describe('Sign In', () => {
+  beforeEach(() => {
+    cy.setUpApi();
+    cy.visit(SIGN_IN_PATH);
+  });
+  it('Can use Enter to validate email', () => {
+    cy.get(`#${MAGIC_LINK_EMAIL_FIELD_ID}`).type(`${MEMBERS.BOB.email}{Enter}`);
+    cy.get(`#${SUCCESS_CONTENT_ID}`).should('be.visible');
   });
 });
--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,70 +1,38 @@
-import React, { FC, useEffect, useState } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 import { useAuthTranslation } from '../config/i18n';
-import { AUTH } from '../langs/constants';
-import { emailValidator } from '../utils/validation';
 import { EmailAdornment } from './common/EmailAdornment';
 import StyledTextField from './common/StyledTextField';
 
-const { EMAIL_INPUT_PLACEHOLDER } = AUTH;
-
 type Props = {
-  required?: boolean;
-  value: string;
   id?: string;
   disabled?: boolean;
-  setValue: (str: string) => void;
-  onKeyPress?: React.KeyboardEventHandler<unknown>;
-  shouldValidate: boolean;
   autoFocus?: boolean;
+  form: UseFormRegisterReturn<'email'>;
+  error?: string;
 };
 
-export const EmailInput: FC<Props> = ({
-  required = false,
-  value = '',
+export const EmailInput = ({
   id,
   disabled = false,
-  setValue,
-  onKeyPress,
-  shouldValidate = true,
   autoFocus = false,
-}) => {
+  form,
+  error,
+}: Props): JSX.Element => {
   const { t } = useAuthTranslation();
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (shouldValidate) {
-      setError(emailValidator(value));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [shouldValidate]);
-
-  const handleEmailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newEmail = e?.target?.value;
-    setValue?.(newEmail);
-    if (shouldValidate) {
-      setError(emailValidator(newEmail));
-    }
-  };
 
   return (
     <StyledTextField
+      id={id}
       InputProps={{
         startAdornment: EmailAdornment,
       }}
       variant="outlined"
-      value={value}
       error={Boolean(error)}
       helperText={error && t(error)}
-      placeholder={t(
-        `${EMAIL_INPUT_PLACEHOLDER}${required ? '_REQUIRED' : ''}`,
-      )}
       autoFocus={autoFocus}
-      onChange={handleEmailOnChange}
-      id={id}
-      type="email"
       disabled={disabled}
-      onKeyPress={onKeyPress}
+      {...form}
     />
   );
 };
